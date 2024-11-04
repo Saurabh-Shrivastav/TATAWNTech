cfb7d47c82740d53de4a5f70ec2476e46974c814
Sᗩᑘᖇᗩᗷᕼ <134703565+Saurabh-Shrivastav@users.noreply.github.com>
2024-11-04 21:37:20 +0530

    Update VIP.jsx
    Amount change in VIP Section

--- frontend/src/Components/VIP.jsx
+++ frontend/src/Components/VIP.jsx
@@ -22,7 +22,7 @@ function VIP() {
                             <img src="https://heraeusinvest.in/static/wap/img/ceS4ZA74eP.png" alt="" />
                         </div>
                         <div className="vip_item">VIP1</div>
-                        <div className="vip_item">400.00</div>
+                        <div className="vip_item">500.00</div>
                     </div>
 
                     <div className="vip_list">
@@ -30,56 +30,56 @@ function VIP() {
                             <img src="https://heraeusinvest.in/static/wap/img/xFw4HZtlYf.png" alt="" />
                         </div>
                         <div className="vip_item">VIP2</div>
-                        <div className="vip_item">2000.00</div>
+                        <div className="vip_item">1600.00</div>
                     </div>
                     <div className="vip_list">
                         <div className="vip_item">
                             <img src="https://heraeusinvest.in/static/wap/img/G4Qcc3jo44.png" alt="" />
                         </div>
                         <div className="vip_item">VIP3</div>
-                        <div className="vip_item">5000.00</div>
+                        <div className="vip_item">2600.00</div>
                     </div>
                     <div className="vip_list">
                         <div className="vip_item">
                             <img src="https://heraeusinvest.in/static/wap/img/j2D5JEASF2.png" alt="" />
                         </div>
                         <div className="vip_item">VIP4</div>
-                        <div className="vip_item">10000.00</div>
+                        <div className="vip_item">5200.00</div>
                     </div>
                     <div className="vip_list">
                         <div className="vip_item">
                             <img src="https://heraeusinvest.in/static/wap/img/Ci3WGuPdl4.png" alt="" />
                         </div>
                         <div className="vip_item">VIP5</div>
-                        <div className="vip_item">20000.00</div>
+                        <div className="vip_item">9300.00</div>
                     </div>
                     <div className="vip_list">
                         <div className="vip_item">
                             <img src="https://heraeusinvest.in/static/wap/img/S5KAcg0qjQ.png" alt="" />
                         </div>
                         <div className="vip_item">VIP6</div>
-                        <div className="vip_item">50000.00</div>
+                        <div className="vip_item">14352.00</div>
                     </div>
                     <div className="vip_list">
                         <div className="vip_item">
                             <img src="https://heraeusinvest.in/static/wap/img/A6PFbYiofX.png" alt="" />
                         </div>
                         <div className="vip_item">VIP7</div>
-                        <div className="vip_item">100000.00</div>
+                        <div className="vip_item">18590.00</div>
                     </div>
                     <div className="vip_list">
                         <div className="vip_item">
                             <img src="https://heraeusinvest.in/static/wap/img/FhyMbP2QtW.png" alt="" />
                         </div>
                         <div className="vip_item">VIP8</div>
-                        <div className="vip_item">500000.00</div>
+                        <div className="vip_item">22000.00</div>
                     </div>
                     <div className="vip_list">
                         <div className="vip_item">
                             <img src="https://heraeusinvest.in/static/wap/img/Uu572N1i2a.png" alt="" />
                         </div>
                         <div className="vip_item">VIP9</div>
-                        <div className="vip_item">1000000.00</div>
+                        <div className="vip_item">45000.00</div>
                     </div>
                 </div>
 
